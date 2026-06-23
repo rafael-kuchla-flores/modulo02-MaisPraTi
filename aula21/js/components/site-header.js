9140f31332651a177f6e3158a7bf6ee8de362339
rafael-kuchla-flores <rafael.kuchla.flores@gmail.com>
2026-06-22 21:40:02 -0300

    Adicionando os components do site-news e estudando um pouco de DOM na aula24

---
+++ aula21/js/components/site-header.js
@@ -0,0 +1,15 @@
+class SiteHeader extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `<header class="cabecalho">
+                <div class="logo">Clarim Diário</div>
+                <nav class="navegacao">
+                    <a href="materia.html">Política</a>
+                    <a href="#">Economia</a>
+                    <a href="#">Esportes</a>
+                    <a href="#">Homem-Aranha</a>
+                </nav>
+            </header>`
+  }
+}
+
+customElements.define('site-header', SiteHeader)
