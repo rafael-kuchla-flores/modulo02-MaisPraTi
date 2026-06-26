62fabe50d3f637995d00495cfad829b1b15acf6e
rafael-kuchla-flores <rafael.kuchla.flores@gmail.com>
2026-06-25 22:07:14 -0300

    Criando a página de cadastro e aprendendo a consumir uma API de cep

---
+++ aula21/js/services/viacep.js
@@ -0,0 +1,22 @@
+export async function buscarCep(cep) {
+  let cepLimpo = cep.replace(/\D/g, '')
+
+  if (cepLimpo.length !== 8) {
+    throw new Error('O CEP deve ter 8 dígitos.')
+  }
+
+  let resposta = await fetch(`https://viacep.com.br/ws/${cepLimpo}/json/`)
+  let dados = await resposta.json()
+
+  if (dados.erro) {
+    throw new Error('CEP não encontrado.')
+  }
+
+  return dados
+}
+
+/*
+  Crie um campo de input de texto - CEP
+
+  - Ao clicar no botão, automaticamente vai preencher os demais campos
+*/
