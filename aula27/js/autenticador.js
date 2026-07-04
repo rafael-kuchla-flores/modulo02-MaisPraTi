a0fdfb85863c220ee7343332164b8604e75539d1
rafael-kuchla-flores <rafael.kuchla.flores@gmail.com>
2026-07-04 09:26:25 -0300

    Adicionando o autenticador do login e cadastro

---
+++ aula27/js/autenticador.js
@@ -0,0 +1,36 @@
+const CHAVE_USUARIOS = 'usuario'
+const CHAVE_SESSAO = 'usuarioLogado'
+
+function lerUsuarios() {
+  const json = localStorage.getItem(CHAVE_USUARIOS)
+  return json ? JSON.parse(json) : []
+}
+
+export function registrar(usuario) {
+  const usuarios = lerUsuarios()
+
+  if (usuarios.some(user => user.email === usuario.email)) {
+    throw new Error('Já existe esse email')
+  }
+
+  usuarios.push(usuario)
+  localStorage.setItem(CHAVE_USUARIOS, JSON.stringify(usuarios))
+}
+
+export function login(email, senha) {
+  const usuarios = lerUsuarios()
+  const usuario = usuarios.find(
+    user => user.email === email && user.senha === senha
+  )
+
+  if (!usuario) {
+    throw new Error('Email ou senha incorretos')
+  }
+  localStorage.setItem(
+    CHAVE_SESSAO,
+    JSON.stringify({
+      email: usuario.email
+    })
+  )
+  return usuario
+}
